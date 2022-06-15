0e2d33a5f5a8bdc94fe931ea90aca0a097401e04
@gilsusana <gilsusanaCIBAF5@gmail.com>
2022-06-15 22:10:35 +0200

    dina

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-function ocultarPrincipal(){
+/*function ocultarPrincipal(){
     document.querySelector(".container-general").style.display="none";
-    document.querySelector(".cajacompra");.style.style.display= "block";
+    document.querySelector(".container-cart").style.style.display= "block";
 }
 
 ocultarPrincipal()
 
 function mostrarPrincipal(){
     document.querySelector(".container-general").style.display="block";
-    document.querySelector(".cajacompra").style.display="none";
+    document.querySelector(".container-cart").style.display="none";
 }
 
 mostrarPrincipal()
