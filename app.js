2327ec794d573a8862c1904333c89f8114a3ff57
@gilsusana <gilsusanaCIBAF5@gmail.com>
2022-06-17 11:17:31 +0200

    b/n

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+let contador = 0;
+function addCounter(){
+    contador = contador + 1;
+    document.querySelector("#counter").innerHTML = contador;
+}
+var contar = 0
+btnElm.onclick = function() {
+contar++;
+}
+
 function ocultarPrincipal(){
     document.querySelector(".container-general").style.display="none";
-    document.querySelector(".container-cart").style.style.display= "block";
+    document.querySelector(".container-cart").style.display= "block";
 }
 
 ocultarPrincipal()
